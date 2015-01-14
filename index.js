fa6f0a885e6f5d022d92314944fa8fec496ee77e
Gideon Rosales <gideonrosales@gmail.com>
2015-01-14 10:49:29 +0800

    feature: add send-receive pattern restructure folder
    - add send-receive pattern
    - restructured folder
    - add standard for message type this is to handle dynamically add exchange and queues
    - add throw error if not valid messageType

--- index.js
+++ index.js
@@ -1,61 +1,21 @@
 'use strict';
 
-// Load core modules
-var util = require( 'util' );
+// Load patterns
+var ReqRes      = require( './lib/req-res' );
+var SendReceive = require( './lib/send-receive' );
 
-// Load third-party modules
-var Rabbus = require( 'rabbus' );
+module.exports = function Lapin ( Rabbit ) {
 
-module.exports = function ( Rabbit ) {
-	var cache = {
-		'producers' : { },
-		'consumers' : { }
-	};
+	// Initialize patterns
+	var reqres      =  new ReqRes( Rabbit );
+	var sendreceive =  new SendReceive( Rabbit );
 
-	function request ( messageType, mesage, callback ) {
-		function Requester ( rabbus ) {
-			Rabbus.Requester.call( this, rabbus, {
-				'exchange'    : 'req-res.exchange',
-				'messageType' : 'req-res.' + messageType
-			} );
-		}
-		util.inherits( Requester, Rabbus.Requester );
-
-		if ( !cache.producers[ messageType ] ) {
-			cache.producers[ messageType ] = new Requester( Rabbit );
-		}
-
-		cache.producers[ messageType ].request( mesage, function ( data, done ) {
-			var error = null;
-
-			if ( data.status !== 'success' ) {
-				error = data;
-				data  = null;
-			}
-
-			callback.call( null, error, data );
-		} );
-	}
-
-	function response ( messageType, callback ) {
-		function Responder ( rabbus ) {
-			Rabbus.Responder.call( this, rabbus, {
-				'exchange'    : 'req-res.exchange',
-				'queue'       : 'req-res.users-queue',
-				'messageType' : 'req-res.' + messageType
-			} );
-		}
-		util.inherits( Responder, Rabbus.Responder );
-
-		if ( !cache.consumers[ messageType ] ) {
-			cache.consumers[ messageType ] = new Responder( Rabbit );
-		}
-
-		cache.consumers[ messageType ].handle( callback );
-	}
-
+	// Export interfaces
 	return {
-		'request'  : request,
-		'response' : response
+		'request'  : reqres.request,
+		'response' : reqres.response,
+		'send'     : sendreceive.send,
+		'receive'  : sendreceive.receive
 	};
+
 };
